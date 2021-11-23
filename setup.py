e6aa7b8eb9461f6ffed1093ed1e1b89f861a60e2
sarnickaa <39015874+sarnickaa@users.noreply.github.com>
2021-11-23 11:16:38 -0500

    fix: trying new version read (#28)
    * fix: trying new version read
    
    * Create version.py

--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
 from setuptools import setup
+from distutils.util import convert_path
 
-__version__ = "0.6.0"
+# __version__ = "0.6.0"
 
+version = {}
+ver_path = convert_path('version.py')
+with open(ver_path) as ver_file:
+    exec(ver_file.read(), version)
+      
 setup(
-   version=__version__,
+   version=version['__version__'],
    name=transformation-generator,
    # And so on...
 )
