0967506c12ad915ed4eeda37bd5e1a16636e88ac
sarnickaa <39015874+sarnickaa@users.noreply.github.com>
2021-12-15 13:48:57 -0500

    fix: deleting file (#35)
    * Delete setup.cfg
    
    * fix: deleting unused file
    
    * fix: updating version-file key
    
    * fix: deleting unused file
    
    * fix: updating setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,12 @@
 from setuptools import setup
 from distutils.util import convert_path
 
-# __version__ = "0.6.0"
-
 version = {}
 ver_path = convert_path('version.py')
 with open(ver_path) as ver_file:
     exec(ver_file.read(), version)
       
 setup(
-   version=version['__version__'],
-   name=transformation-generator,
+   version=version['__version__']
    # And so on...
 )
